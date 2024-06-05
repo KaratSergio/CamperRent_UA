876c641a6ee6c2139cc53720463b6e2b25fd6e1d
KaratSergio <137483700+KaratSergio@users.noreply.github.com>
2024-06-05 03:11:59 +0000

    Deploying to gh-pages from @ KaratSergio/CamperRent_UA@bbdd6052ac6cd3fa899340bef6379b7ba9c64238 🚀

---
+++ assets/NotFound-f50777cd.js
@@ -0,0 +1,15 @@
+import{s as n,j as e,I as t}from"./index-06cd864f.js";const o="/CamperRent_UA/assets/camp-63f245ee.png",r=n.div`
+  justify-content: space-around;
+  background-image: url('${o}');
+  background-size: 20%;
+  background-repeat: no-repeat;
+  background-position-x: 1230px;
+  width: 100%;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 46px;
+  margin-left: -260px;
+`,i=()=>e.jsxs(r,{children:[e.jsx("h1",{children:"Page not found"}),e.jsx("p",{children:"404"}),e.jsxs("a",{href:"/",children:["← return to ",e.jsx(t,{id:"icon-home",width:32,height:32})," Home page"]})]});export{i as default};
